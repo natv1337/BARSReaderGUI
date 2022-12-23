130982250f2961389f3bbead9d1321c4a8cb3821
August <faintlightswitch@gmail.com>
2022-12-23 21:50:44 +0000

    AMTA v5 reader

--- BARSReaderGUI/AMTA.cs
+++ BARSReaderGUI/AMTA.cs
@@ -6,34 +6,66 @@ using System.Threading.Tasks;
 
 namespace BARSReaderGUI
 {
+    public enum AMTAVersion : ushort
+    {
+        V1 = 0x0100,
+        V3 = 0x0300,
+        V4 = 0x0400,
+        V5 = 0x0500
+    }
     public class AMTA //Audio Metadata
     {
-        public string magic; //always AMTA
-        public ushort endian;
-        public ushort version;
-        public uint size;
-        public uint unk1;
-        public uint unk2; //observed 0x34 or 0x38
-        public uint unk3;
-        public uint unk4;
-        public uint unk5;
-        public uint unk6;
-        public string assetname;
-    }
-    public class AMTADATA
-    {
-        public uint size;
-        public uint namehash; //same as asset name hash
-        public uint unk1; //type?
-        public byte unk2;
-        public byte channelcount;
-    }
+        public void ReadAMTA(uint startPosition, NativeReader reader)
+        {
+            reader.Position = startPosition;
+            string magic = reader.ReadSizedString(4); //always AMTA
+            ushort endian = reader.ReadUShort(); //0xFFFE for little, 0xFEFF for big
+            ushort version = reader.ReadUShort();
+            uint size = reader.ReadUInt();
 
-    public class MINF //Music Info
-    {
-        public string magic; //always MINF
-        public ushort endian;
-        public ushort version;
-        public uint size;
+            switch ((AMTAVersion)version)
+            {
+                case AMTAVersion.V5:
+                    {
+                        ReadAMTAV5(startPosition, reader);
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Unsupported AMTA version.");
+                    return;
+            }
+        }
+
+        #region V5
+        public void ReadAMTAV5(uint startPosition, NativeReader reader)
+        {
+            uint unk1 = reader.ReadUInt();
+            uint unk2 = reader.ReadUInt(); //observed 0x34 or 0x38
+            uint unk3 = reader.ReadUInt();
+            uint unk4 = reader.ReadUInt();
+            uint unk5 = reader.ReadUInt();
+            uint unk6 = reader.ReadUInt();
+            ReadAMTADATAV5(startPosition, reader);
+            string assetname = reader.ReadNullTerminatedString();
+        }
+        public void ReadAMTADATAV5(uint startPosition, NativeReader reader)
+        {
+            reader.Position = startPosition;
+            uint datasize = reader.ReadUInt();
+            uint namehash = reader.ReadUInt(); //same as asset name hash
+            uint unk1 = reader.ReadUInt();
+            byte unk2 = reader.ReadByte();
+            byte channelcount = reader.ReadByte();
+            reader.Position = startPosition + datasize;
+        }
+
+        //public class MINF //Music Info
+        //{
+        //    public string magic; //always MINF
+        //    public ushort endian;
+        //    public ushort version;
+        //    public uint size;
+        //}
     }
+    #endregion
 }
